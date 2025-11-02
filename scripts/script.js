bda4121c3cffd39e7b523fcf5b9898196d464eed
mmizumoto1213 <mmizumoto@ucsd.edu>
2025-11-02 05:49:54 -0800

    Added Education next and previous page buttons

--- scripts/script.js
+++ scripts/script.js
@@ -3,39 +3,49 @@ document.getElementById('js-linkedin-button').addEventListener('click', function
   window.open('https://www.linkedin.com/in/matthew-mizumoto-750285252/', '_blank');
 });
 
-document.getElementById('js-github-button').addEventListener('click', function() {
+document.getElementById('js-resume-button').addEventListener('click', function() {
   // This will open the link in a new tab
-  window.open('https://github.com/mmizumoto1213', '_blank');
+  window.open('./pdfs/resume.pdf', '_blank');
 });
 
-document.getElementById('js-intro').addEventListener('click', function() {
-  // This will open the introduction section
-  window.location.href = 'index.html';
+const intro = document.querySelectorAll('.js-intro');
+intro.forEach(element => {
+  element.addEventListener('click', function() {
+    // This will open the introduction section
+    window.location.href = 'index.html';
+  });
 });
 
-document.getElementById('js-about').addEventListener('click', function() {
-  // This will open the about me section
-  window.location.href = 'about.html';
+const about = document.querySelectorAll('.js-about');
+about.forEach(element => {
+  element.addEventListener('click', function() {
+    // This will open the about me section
+    window.location.href = 'about.html';
+  });
 });
 
-document.getElementById('js-projects').addEventListener('click', function() {
-  // This will open the projects section
-  window.location.href = 'projects.html';
+const projects = document.querySelectorAll('.js-projects');
+projects.forEach(element => {
+  element.addEventListener('click', function() {
+    // This will open the projects section
+    window.location.href = 'projects.html';
+  });
 });
 
-document.getElementById('js-skills').addEventListener('click', function() {
-  // This will open the skills & tools section
-  window.location.href = 'skills.html';
-}); 
-
-document.getElementById('js-education').addEventListener('click', function() {
-  // This will open the education section
-  window.location.href = 'education.html';
+const skills = document.querySelectorAll('.js-skills');
+skills.forEach(element => {
+  element.addEventListener('click', function() {
+    // This will open the projects section
+    window.location.href = 'skills.html';
+  });
 });
 
-document.getElementById('js-matthew-mizumoto').addEventListener('click', function() {
-  // This will open the introduction section
-  window.location.href = 'index.html';
+const education = document.querySelectorAll('.js-education');
+education.forEach(element => {
+  element.addEventListener('click', function() {
+    // This will open the projects section
+    window.location.href = 'education.html';
+  });
 });
 
 document.getElementById("js-pantrypal").addEventListener("click", function() {
@@ -176,7 +186,7 @@ document.getElementById("js-live-asl-translator").addEventListener("click", func
   });
 
   document.getElementById('js-report-link').addEventListener('click', function() {
-    window.open('asl_report.pdf', '_blank');
+    window.open('./pdfs/asl_report.pdf', '_blank');
   });
 });
 
